94bcb31517b28b1718cecf629eac462acec95850
Shibu <shibum123@gmail.com>
2019-02-26 11:30:37 +0000

    initial commit

---
+++ src/components/Stats.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="stats">
+    <h1> New component </h1>
+    {{ countLinks }}
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Stats',
+  computed: {
+    ...mapGetters([
+      'countLinks'
+    ])
+  }
+}
+</script>
